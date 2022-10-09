7d72276baee8b0b879770af0db1c85f06e6612d7
Ibuki-Y <ibu.love.drum@gmail.com>
2022-10-10 08:09:28 +0900

    Add useMutation for task

---
+++ hooks/useQueryTasks.ts
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router';
+import { useQuery } from '@tanstack/react-query';
+import { Task } from '@prisma/client';
+import axios from 'axios';
+
+export const useQueryTasks = () => {
+  const router = useRouter();
+  const getTasks = async () => {
+    const { data } = await axios.get<Task[]>(
+      `${process.env.NEXT_PUBLIC_API_URL}/todo`
+    );
+
+    return data;
+  };
+
+  return useQuery<Task[], Error>({
+    queryKey: ['tasks'],
+    queryFn: getTasks,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403) {
+        router.push('/');
+      }
+    },
+  });
+};
